c8775e06004c3f85aacee60027dc4bd8e4adb0db
ester826 <costaester140@gmail.com>
2024-09-09 16:13:46 -0300

    Add files via upload

--- exercicios3.js
+++ exercicios3.js
@@ -38,6 +38,7 @@ c1 = c1 / 2;
 
 console.log(a1, b1, c1);
 
+//20, 2, 7.5
 
 //2. Analise o programa abaixo e diga o que será impresso no console, SEM EXECUTAR o programa.
 
@@ -51,6 +52,7 @@ let resultado3_2 = !(a2 < c2);
 
 console.log(resultado1_2, resultado2_2, resultado3_2);
 
+//true, true, false 
 
 //3. Analise o programa abaixo e diga o que será impresso no console, SEM EXECUTAR o programa.
 
@@ -64,6 +66,7 @@ let comparacao3_3 = b3 <= a3 && !(c3 == 5);
 
 console.log(comparacao1_3, comparacao2_3, comparacao3_3);
 
+//true, true, false
 
 //4. Analise o programa abaixo e diga o que será impresso no console, SEM EXECUTAR o programa.
 
@@ -76,6 +79,7 @@ let resultadoC_4 = (a4 - b4) * 2 == 22;
 
 console.log(resultadoA_4, resultadoB_4, resultadoC_4);
 
+//true, true, true
 
 //5. Analise o programa abaixo e diga o que será impresso no console, SEM EXECUTAR o programa.
 
@@ -89,6 +93,7 @@ let resultado3_5 = !(a5 % b5 == 0) || c5 == 3;
 
 console.log(resultado1_5, resultado2_5, resultado3_5);
 
+//true, false, true
 
 //6. Analise o programa abaixo e diga o que será impresso no console, SEM EXECUTAR o programa.
 
@@ -100,6 +105,7 @@ let e6 = b6.toString();
 
 console.log(typeof a6, typeof b6, typeof c6, typeof d6, typeof e6);
 
+//string, number, string, number, string
 
 //7. Analise o programa abaixo e diga o que será impresso no console, SEM EXECUTAR o programa.
 
@@ -113,6 +119,7 @@ let resultado3_7 = (c7 > 50) && (typeof c7 == "number");
 
 console.log(resultado1_7, resultado2_7, resultado3_7);
 
+//false, false, true
 
 //8. Analise o programa abaixo e diga o que será impresso no console, SEM EXECUTAR o programa.
 
@@ -127,6 +134,7 @@ let resultadoC_8 = d8 === a8 + b8.toString();
 
 console.log(resultadoA_8, resultadoB_8, resultadoC_8);
 
+//true, true, true
 
 //9. Analise o programa abaixo e diga o que será impresso no console, SEM EXECUTAR o programa.
 
@@ -141,6 +149,7 @@ let resultado3_9 = Number(a9) + b9 == c9 * b9;
 
 console.log(resultado1_9, resultado2_9, resultado3_9);
 
+//true, true, false
 
 //10. Analise o programa abaixo e diga o que será impresso no console, SEM EXECUTAR o programa.
 
@@ -155,6 +164,7 @@ let resultado3_10 = c10 == Number(d10);
 
 console.log(resultado1_10, resultado2_10, resultado3_10);
 
+//true, false, true
 
 /*
 █████████████████████████████████████████████████████████████████████████████████████████████████████████
@@ -186,32 +196,67 @@ console.log(resultado1_10, resultado2_10, resultado3_10);
 // que armazene a soma de a11 e b11, multiplicada por c11.
 // Exiba o valor de resultado11 no console.
 
+let a11
+let b11 
+let c11 
 
+a11 = 2
+b11 = 5
+c11 = 7
 
+let resultado11 = (a11 +  b11) * c11
+
+console.log(resultado11)
+
+//49
 
 //12.
 // Crie duas variáveis, x12 e y12, e atribua a elas valores inteiros diferentes.
 // Escreva expressões que comparem se x12 é maior que y12, se y12 é menor ou igual a x12,
 // e se x12 e y12 são iguais. Exiba os resultados dessas comparações no console.
 
+let x12 
+let y12
 
+x12 = 10
+y12 = 20
 
+console.log(x12 > y12 <=x12)
+console.log(x12 === y12)
+
+//true, false
 
 //13.
 // Crie uma variável para armazenar sua idade e outra para armazenar o ano atual.
 // Em seguida, subtraia a idade do ano atual e converta o resultado para string.
 // Exiba o resultado no console.
 
+let idade = 15
+let anoAtual = 2024
+let equaçao = anoAtual - idade 
+
+console.log(equaçao)
+console.log(equaçao.toString())
 
 
+//"2009"
 
 //14.
 // Declare duas variáveis, s14 e t14. Atribua um valor numérico a s14 e uma string numérica a t14.
 // Converta t14 para número usando Number() e adicione-a a s14.
 // Exiba o resultado da adição e o tipo da variável resultante no console usando typeof.
 
+let s14 
+let t14 
 
+s14 = 15 
+t14 = "20" 
 
+let equaçao2 = Number (t14) + s14
+
+console.log(typeof equaçao2)
+
+//35, number 
 
 //15.
 // Crie duas variáveis, u15 e v15, uma com um número e a outra com uma string que representa o mesmo número.
@@ -219,6 +264,14 @@ console.log(resultado1_10, resultado2_10, resultado3_10);
 // Depois, verifique se as variáveis têm o mesmo valor usando == e ===.
 
 
+let u15 = 12
+let v15 = "12" 
+
+console.log( typeof u15 )    //number
+console.log( typeof v15)     //string
+
+console.log( u15 === v15 )   //false
+console.log( u15 == v15)     //true
 
 
 //16.
@@ -226,15 +279,27 @@ console.log(resultado1_10, resultado2_10, resultado3_10);
 // Crie uma variável que armazena a quantidade comprada desse produto.
 // Multiplique os valores e exiba o resultado no console, convertendo-o para string.
 
+const produto = 13
+let compras = 30
 
+let resuldados = produto * compras
 
+console.log(resuldados)
+console.log(resuldados.toString())
+
+//"390"
 
 //17.
 // Crie duas variáveis, z17 e a17, uma com uma string e outra com um número.
 // Concatene as duas variáveis e exiba o resultado e o tipo da variável resultante no console.
 
+let z17 = "suco"
+let a17 = 45
 
+let resuldados4 = z17 + a17
+console.log(resuldados4)
 
+//suco45
 
 //18.
 // Declare três variáveis, b18, c18, e d18, com valores numéricos,
@@ -242,37 +307,57 @@ console.log(resultado1_10, resultado2_10, resultado3_10);
 // Converta as variáveis para o tipo correto e calcule a média desses valores.
 // Exiba a média no console.
 
+let b18 = 2
+let c18 = "4"
+let d18 = "6"
 
+let resuldados2 = b18 + Number(c18) + Number(d18)
+console.log(resuldados2) 
+console.log(typeof resuldados2)
 
+//number, 12
 
 //19.
 // Declare uma variável com um valor numérico e converta-a para uma string usando toString().
 // Depois, converta a string de volta para número usando Number() e exiba o tipo da variável em cada etapa no console.
 
-
-
+let numerico = 24 
+console.log(numerico.toString())
+console.log(typeof numerico)  //string
+console.log(Number (numerico)) 
+console.log(typeof numerico)   //number
 
 //20.
 // Crie uma variável para armazenar a quantidade de horas trabalhadas e outra para armazenar o valor pago por hora.
 // Calcule o total a receber e verifique se o valor é maior que 500.
 // Exiba a resposta no console.
 
+let horasTrabalhadas = 20 
+let valorPago = 100 
+let total = horasTrabalhadas * valorPago 
+console.log(total > 500) 
 
+//true
 
 
 //21.
 // Declare uma constante com um valor booleano qualquer.
 // Em seguida, converta esse valor para número e exiba o valor convertido no console.
 
+const booleano = true 
+const valor = Number(booleano) 
+console.log(valor)
 
-
+//1
 
 //22.
 // Declare uma constante para armazenar seu nome completo.
 // Em seguida, crie uma variável que armazene a quantidade de caracteres desse nome.
 // Exiba a quantidade de caracteres no console.
 
-
+const nomeCompleto = "ester costa" 
+let caracteres = 10 
+console.log(caracteres)
 
 
 //23.
@@ -281,3 +366,9 @@ console.log(resultado1_10, resultado2_10, resultado3_10);
 // Exiba a resposta no console.
 
 
+let num = 28 
+let num2 = 59 
+let numTotal = num - num2 
+console.log(numTotal) 
+
+//-31
